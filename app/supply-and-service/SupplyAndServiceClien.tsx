6700b884b721867852893eba39d7d1d5fb5dd72c
LAPTOP-NU204II8\pongs <dev.pongsiri@gmail.com>
2025-10-02 21:56:01 +0700

    seo

---
+++ app/supply-and-service/SupplyAndServiceClien.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SupplyAndServiceClien = () => {
+  return (
+    <div>
+      This is Supply & Serice Page.
+    </div>
+  )
+}
+
+export default SupplyAndServiceClien
--- app/supply-and-service/page.tsx
+++ app/supply-and-service/page.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import EquipmentClient from "./EquipmentClient";
+import SupplyAndServiceClien from "./SupplyAndServiceClien";
 
 export const metadata: Metadata = {
-  title: "Equipment",
+  title: "Supply and Service",
   description:
     "Explore certified medical equipment from leading global brands with responsive after-sales service.",
-  alternates: { canonical: "/equipment" },
+  alternates: { canonical: "/supply-and-service" },
   robots: { index: true, follow: true },
-  openGraph: { title: "MBD Healthcare / Equipment", url: "/equipment" },
+  openGraph: { title: "MBD Healthcare / Supply&Service", url: "/supply-and-service" },
   twitter: { card: "summary_large_image" },
 };
 
 export default function Page() {
-  return <EquipmentClient />;
+  return <SupplyAndServiceClien />;
 }
